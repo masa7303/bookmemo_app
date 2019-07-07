01f3c59ef74649cf03b3f7b1191c68f9cacc0c14
saeki <shingen7303@gmail.com>
2019-07-07 10:09:30 +0900

    update book newform view

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_05_042002) do
+ActiveRecord::Schema.define(version: 2019_07_06_092911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
+    t.string "author"
+    t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
