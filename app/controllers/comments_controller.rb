05f8c59f5fd82e9539341070beb76f48344800a0
saeki <shingen7303@gmail.com>
2019-07-13 13:27:06 +0900

    comment view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   protect_from_forgery
-  before_action :set_post
+  before_action :set_post, except: [:index]
 
   def new
     @comment = Comment.new(posted_at: Time.current)
@@ -14,6 +14,20 @@ class CommentsController < ApplicationController
   def destroy
     @book.comments.destroy(params[:id])
     redirect_to @book
+  end
+
+  def index
+    @comments = Comment.all
+    # 今日書いた記録
+    @comments1 = @comments.created_today.order(posted_at: :desc)
+    # 昨日書いた記録
+    @comments2 = @comments.created_yesterday.order(posted_at: :desc)
+    # 三日前に書いた記録
+    @comments3 = @comments.created_three_days_ago.order(posted_at: :desc)
+    # 一週間前に書いた記録
+    @comments4 = @comments.created_a_week_ago.order(posted_at: :desc)
+    # 一ヶ月前に書いた記録
+    @comments5 = @comments.created_a_month_ago.order(posted_at: :desc)
   end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     resources :comments, :only => [:create, :destroy]
   end
 
+  resources :comments, :only => [:index]
   root 'top#index'
   get 'about/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
