9e6900b22f8f2af6c55df2b6d6ffd024b7f37248
saeki <shingen7303@gmail.com>
2020-03-01 17:32:34 +0900

    スクロールしたらヘッダー背景色追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,3 +48,17 @@ $(function(){
    });
   });
 });
+
+//ヘッダースクロールイベント
+$(function(){
+  var header = $('.humburger_menu_bg')
+  header_offset = header.offset();
+  header_height = header.height();
+  $(window).scroll(function () {
+    if($(window).scrollTop() > header_offset.top + header_height) {
+      header.addClass('scroll');
+    }else {
+      header.removeClass('scroll');
+    }
+  });
+});
